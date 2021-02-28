5724944bf091a910d73de9fa41c56c7a2cd5b5a8
Ivan Trubchenko <74150435+shpaktakur1@users.noreply.github.com>
2021-02-28 13:53:25 +0300

    Add files via upload

---
+++ TextProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKaspersky
+{
+    class TextProcess : AbsrtactFileProcessor
+    {
+        protected override string GetFormat() => ".txt";
+
+        protected override void StartWork(string fileName)
+        {
+            Console.WriteLine("Работа с txt файлом.");
+        }
+    }
+}
